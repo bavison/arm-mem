e5fbc93de41c26def9a11013d171ae5e221480ba
Ben Avison <bavison@riscosopen.org>
2015-02-26 16:53:10 +0000

    Abstract out cacheline length in preload macros
    This is in preparation for supporting CPUs with differing cacheline lengths.

--- arm-mem.h
+++ arm-mem.h
@@ -32,30 +32,30 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 fname:
 .endm
 
-.macro preload_leading_step1  backwards, ptr, base
-/* If the destination is already 16-byte aligned, then we need to preload
+.macro preload_leading_step1  backwards, ptr, base, log2cl
+/* If the destination is already write-block aligned, then we need to preload
  * between 0 and prefetch_distance (inclusive) cache lines ahead so there
  * are no gaps when the inner loop starts.
  */
  .if backwards
         sub     ptr, base, #1
-        bic     ptr, ptr, #31
+        bic     ptr, ptr, #(1<<log2cl)-1
  .else
-        bic     ptr, base, #31
+        bic     ptr, base, #(1<<log2cl)-1
  .endif
  .set OFFSET, 0
  .rept prefetch_distance+1
         pld     [ptr, #OFFSET]
   .if backwards
-   .set OFFSET, OFFSET-32
+   .set OFFSET, OFFSET-(1<<log2cl)
   .else
-   .set OFFSET, OFFSET+32
+   .set OFFSET, OFFSET+(1<<log2cl)
   .endif
  .endr
 .endm
 
-.macro preload_leading_step2  backwards, ptr, base, leading_bytes, tmp
-/* However, if the destination is not 16-byte aligned, we may need to
+.macro preload_leading_step2  backwards, ptr, base, log2cl, leading_bytes, tmp
+/* However, if the destination is not write-block aligned, we may need to
  * preload one more cache line than that. The question we need to ask is:
  * are the leading bytes more than the amount by which the source
  * pointer will be rounded down for preloading, and if so, by how many
@@ -65,42 +65,42 @@ fname:
 /* Here we compare against how many bytes we are into the
  * cache line, counting down from the highest such address.
  * Effectively, we want to calculate
- *     leading_bytes = dst&15
- *     cacheline_offset = 31-((src-leading_bytes-1)&31)
+ *     leading_bytes = dst&(writeblock-1)
+ *     cacheline_offset = (cacheline-1)-((src-leading_bytes-1)&(cacheline-1))
  *     extra_needed = leading_bytes - cacheline_offset
  * and test if extra_needed is <= 0, or rearranging:
- *     leading_bytes + (src-leading_bytes-1)&31 <= 31
+ *     leading_bytes + (src-leading_bytes-1)&(cacheline-1) <= (cacheline-1)
  */
-        mov     tmp, base, lsl #32-5
-        sbc     tmp, tmp, leading_bytes, lsl #32-5
-        adds    tmp, tmp, leading_bytes, lsl #32-5
+        mov     tmp, base, lsl #32-2log2cl
+        sbc     tmp, tmp, leading_bytes, lsl #32-log2cl  @ requires C clear (borrow set) on entry
+        adds    tmp, tmp, leading_bytes, lsl #32-log2cl
         bcc     61f
-        pld     [ptr, #-32*(prefetch_distance+1)]
+        pld     [ptr, #-(1<<log2cl)*(prefetch_distance+1)]
  .else
 /* Effectively, we want to calculate
- *     leading_bytes = (-dst)&15
- *     cacheline_offset = (src+leading_bytes)&31
+ *     leading_bytes = (-dst)&(writeblock-1)
+ *     cacheline_offset = (src+leading_bytes)&(cacheline-1)
  *     extra_needed = leading_bytes - cacheline_offset
  * and test if extra_needed is <= 0.
  */
-        mov     tmp, base, lsl #32-5
-        add     tmp, tmp, leading_bytes, lsl #32-5
-        rsbs    tmp, tmp, leading_bytes, lsl #32-5
+        mov     tmp, base, lsl #32-log2cl
+        add     tmp, tmp, leading_bytes, lsl #32-log2cl
+        rsbs    tmp, tmp, leading_bytes, lsl #32-log2cl
         bls     61f
-        pld     [ptr, #32*(prefetch_distance+1)]
+        pld     [ptr, #(1<<log2cl)*(prefetch_distance+1)]
  .endif
 61:
 .endm
 
-.macro preload_trailing  backwards, base, remain, tmp
+.macro preload_trailing  backwards, base, log2cl, remain, tmp
         /* We need either 0, 1 or 2 extra preloads */
  .if backwards
         rsb     tmp, base, #0
-        mov     tmp, tmp, lsl #32-5
+        mov     tmp, tmp, lsl #32-log2cl
  .else
-        mov     tmp, base, lsl #32-5
+        mov     tmp, base, lsl #32-log2cl
  .endif
-        adds    tmp, tmp, remain, lsl #32-5
+        adds    tmp, tmp, remain, lsl #32-log2cl
         adceqs  tmp, tmp, #0
         /* The instruction above has two effects: ensures Z is only
          * set if C was clear (so Z indicates that both shifted quantities
@@ -109,36 +109,36 @@ fname:
         beq     82f
  .if backwards
         sub     tmp, base, #1
-        bic     tmp, tmp, #31
+        bic     tmp, tmp, #(1<<log2cl)-1
  .else
-        bic     tmp, base, #31
+        bic     tmp, base, #(1<<log2cl)-1
  .endif
         bcc     81f
  .if backwards
-        pld     [tmp, #-32*(prefetch_distance+1)]
+        pld     [tmp, #-(1<<log2cl)*(prefetch_distance+1)]
 81:
-        pld     [tmp, #-32*prefetch_distance]
+        pld     [tmp, #-(1<<log2cl)*prefetch_distance]
  .else
-        pld     [tmp, #32*(prefetch_distance+2)]
+        pld     [tmp, #(1<<log2cl)*(prefetch_distance+2)]
 81:
-        pld     [tmp, #32*(prefetch_distance+1)]
+        pld     [tmp, #(1<<log2cl)*(prefetch_distance+1)]
  .endif
 82:
 .endm
 
-.macro preload_all    backwards, narrow_case, shift, base, remain, tmp0, tmp1
+.macro preload_all    backwards, narrow_case, shift, base, log2cl, remain, tmp0, tmp1
  .if backwards
         sub     tmp0, base, #1
-        bic     tmp0, tmp0, #31
+        bic     tmp0, tmp0, #(1<<log2cl)-1
         pld     [tmp0]
         sub     tmp1, base, remain, lsl #shift
  .else
-        bic     tmp0, base, #31
+        bic     tmp0, base, #(1<<log2cl)-1
         pld     [tmp0]
         add     tmp1, base, remain, lsl #shift
         sub     tmp1, tmp1, #1
  .endif
-        bic     tmp1, tmp1, #31
+        bic     tmp1, tmp1, #(1<<log2cl)-1
         cmp     tmp1, tmp0
         beq     92f
  .if narrow_case
@@ -147,9 +147,9 @@ fname:
  .else
 91:
   .if backwards
-        sub     tmp0, tmp0, #32
+        sub     tmp0, tmp0, #1<<log2cl
   .else
-        add     tmp0, tmp0, #32
+        add     tmp0, tmp0, #1<<log2cl
   .endif
         cmp     tmp0, tmp1
         pld     [tmp0]
