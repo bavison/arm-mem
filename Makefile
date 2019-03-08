2a95bbea558beb9ef9accf3a6cea32fd2c7909ec
Ben Avison <bavison@riscosopen.org>
2019-03-08 14:24:42 +0000

    Test version to determine callers of memcmp()

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
 OBJS-V6L = memcmp-v6l.o memcpymove-v6l.o memset-v6l.o
-OBJS-V7L = memcmp-v7l.o memcpymove-v7l.o memset-v7l.o
-CFLAGS += -std=gnu99 -O2 -fno-inline
+OBJS-V7L = memcmp-log.o memcpymove-v7l.o memset-v7l.o
+CFLAGS += -g -std=gnu99 -O2 -fno-inline
+CPPFLAGS += -g -O2
 
 all: libarmmem-v6l.so libarmmem-v6l.a libarmmem-v7l.so libarmmem-v7l.a test
+
+%.o: %.cpp
+	$(CROSS_COMPILE)g++ $(CPPFLAGS) -c -o $@ $^
 
 %.o: %.c
 	$(CROSS_COMPILE)gcc $(CFLAGS) -c -o $@ $^
 
 %.o: %.S
-	$(CROSS_COMPILE)gcc -c -o $@ $^
+	$(CROSS_COMPILE)gcc -g -c -o $@ $^
 
 libarmmem-v6l.so: $(OBJS-V6L)
 	$(CROSS_COMPILE)gcc -shared -o $@ $^
@@ -17,7 +21,7 @@ libarmmem-v6l.a: $(OBJS-V6L)
 	$(CROSS_COMPILE)ar rcs $@ $^
 
 libarmmem-v7l.so: $(OBJS-V7L)
-	$(CROSS_COMPILE)gcc -shared -o $@ $^
+	$(CROSS_COMPILE)gcc -shared -o $@ $^ -lstdc++
 
 libarmmem-v7l.a: $(OBJS-V7L)
 	$(CROSS_COMPILE)ar rcs $@ $^
---
+++ memcmp-log.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <mutex>
+#include <string>
+
+#include <cstdlib>
+#include <cstddef>
+#include <cstdint>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+static std::mutex mutex;
+static bool registered;
+static std::map<void *, uint32_t> map;
+
+extern "C" int neon_memcmp(const void *s1, const void *s2, size_t n);
+
+static void myatexit(void)
+{
+    /* Make a container of memcmp caller addresses, sorted by frequency */
+    mutex.lock();
+    std::multimap<uint32_t, void *> by_freq;
+    std::transform(map.begin(), map.end(), std::inserter(by_freq, by_freq.begin()),
+            [](const std::pair<void *, uint32_t> &p) {
+        return std::pair<uint32_t, void *>(p.second, p.first);
+    });
+    mutex.unlock();
+
+    for (auto it = by_freq.rbegin(); it != by_freq.rend(); it++)
+    {
+        /* The invocation of addr2line appears to call memcmp once; avoid recursion by filtering these out */
+        if (it->first > 1)
+        {
+            char cmd[1024];
+            printf("%u calls from:\n", it->first);
+            snprintf(cmd, 1024, "addr2line -C -e /proc/%d/exe -f %p", getpid(), it->second);
+            system(cmd);
+        }
+    }
+}
+
+extern "C" int memcmp(const void *s1, const void *s2, size_t n)
+{
+    mutex.lock();
+    if (!registered++)
+        atexit(myatexit);
+    ++map[__builtin_return_address(0)];
+    mutex.unlock();
+
+    int result = 0;
+    const uint8_t *p1 = (const uint8_t *) s1, *p2 = (const uint8_t *) s2;
+    for (; !result && n--; p1++, p2++)
+        result = *p1 - *p2;
+    return result;
+}
